ebdebd58dca2160ff2a06e60382ab6a2532b08d8
6141 <72698108+6141@users.noreply.github.com>
2024-01-13 13:09:54 +0530

    Update create-form.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -122,13 +122,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
           </div>
         </fieldset>
-      </div>
-      {state.errors?.status &&
+        {state.errors?.status &&
           state.errors.status.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
               {error}
             </p>
           ))}
+      </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
